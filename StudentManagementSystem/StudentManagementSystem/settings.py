91590c7353a77b906dcd907ebfac151310e3eb11
Islam <kogsaitama@gmail.com>
2024-11-12 21:59:16 +0500

    create apps and imports

--- StudentManagementSystem/StudentManagementSystem/settings.py
+++ StudentManagementSystem/StudentManagementSystem/settings.py
@@ -37,6 +37,19 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #apps
+    'users',
+    'students',
+    'notifications',
+    'grades',
+    'courses',
+    'attendance',
+
+
+    #imports
+    'rest_framework',
+    'djcelery',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +134,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
